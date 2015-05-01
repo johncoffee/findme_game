06e4b4f0ecfb0a2df677553d6afdcf6ef9436fd5
Matthias Maschek <matthiasmaschek@gmail.com>
2015-05-01 15:07:17 +0200

    atopping coroutine

--- Assets/scripts/ITimeupEvent.cs
+++ Assets/scripts/ITimeupEvent.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 public interface ITimeupEvent : IEventSystemHandler {
 
 	void Timeup();
+	void Found();
 
 
 }
--- Assets/scripts/Sounds.cs
+++ Assets/scripts/Sounds.cs
@@ -1,14 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Sounds : MonoBehaviour, ITimeupEvent {
 
 	public AudioSource timeoutBuzz = null;
+	public List<AudioSource> ambientSounds = new List<AudioSource>();
 
 	public void Timeup ()
 	{
 		if (timeoutBuzz != null)
 		timeoutBuzz.Play();
+		StopAmbient ();
+	}
+	public void Found ()
+	{
+		StopAmbient ();
+	}
+
+
+	public void PlayAmbient(int roomID) {
+		ambientSounds [roomID - 1].Play ();
+	}
+
+	public void StopAmbient() {
+		foreach (AudioSource source in ambientSounds) {
+			source.Stop();
+		}
 	}
 
 }
