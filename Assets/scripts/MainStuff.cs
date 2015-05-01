70c892ec3596c710d412087cc27e4ecd1fe64c5a
Julian <julian@contingent.dk>
2015-05-01 12:29:29 +0200

    timers

---
+++ Assets/scripts/MainStuff.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainStuff : MonoBehaviour {
+
+	public Timers timers;
+
+	// Use this for initialization
+	void Start () {
+		timers = GetComponent<Timers>();
+
+
+		CheckinRoom(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CheckinRoom (int roomID) {
+
+		switch (roomID) {
+			case 1:
+				timers.CreateTimer (2f, () => {
+					Debug.Log("Button 1 has timeout");
+				});
+				break;	
+
+		}
+	}
+	
+}
---
+++ Assets/scripts/Timers.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timers : MonoBehaviour {
+
+	float time;
+
+	public delegate void OnTimer();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CreateTimer (float seconds, OnTimer callback) {
+		StartCoroutine(createTimer(seconds, callback));
+	}
+
+	IEnumerator createTimer(float seconds, OnTimer callback) {
+		yield return new WaitForSeconds(seconds);
+		if(callback != null) 
+			callback();
+	}
+
+
+}
