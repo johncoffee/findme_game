f529ea3c64d68d3f21102de99bdc5d9e35151464
Julian <julian@contingent.dk>
2015-05-01 13:01:45 +0200

    added active room state

--- Assets/scripts/MainStuff.cs
+++ Assets/scripts/MainStuff.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class MainStuff : MonoBehaviour {
 
 	public Timers timers;
+	bool bodyPartWasFound= false;
+
+	public HashSet<int> visitedRooms = new HashSet<int>();
+
+	public int activeRoomID = -1;
 
 	// Use this for initialization
 	void Start () {
@@ -14,18 +20,29 @@ public class MainStuff : MonoBehaviour {
 		CheckinRoom(1);
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
+	public void Found () {
+		bodyPartWasFound = true;
+		visitedRooms.Add(activeRoomID);
+	}
+
+
 	public void CheckinRoom (int roomID) {
+		bodyPartWasFound = false;
+
+		activeRoomID = roomID;
 
 		switch (roomID) {
 			case 1:
-				timers.CreateTimer (2f, () => {
-					Debug.Log("Button 1 has timeout");
-					ExecuteEvents.Execute<ITimeupEvent>(gameObject, null, (x,y) => x.Timeup() );			
+				timers.CreateTimer (2f, () => {					
+					if (!bodyPartWasFound) {
+						ExecuteEvents.Execute<ITimeupEvent>(gameObject, null, (x,y) => x.Timeup() );			
+					}
 				});
 				break;	
 
